dd98e173983ee2eb742989af549fd7fc0fbd46c3
pleewson <adrianplewait@gmail.com>
2024-01-20 19:12:26 +0100

    in progress

---
+++ src/main/java/database/connection/DbUtil.java
@@ -0,0 +1,17 @@
+package database.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DbUtil {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/workshop2";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "root2612";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+    }
+
+}
---
+++ src/main/java/entity/TaskManager.java
@@ -0,0 +1,9 @@
+package entity;
+
+public class TaskManager {
+
+    public static void main(String[] args) {
+        UserDAO.createUser("testemail1@gmail.com", "testUser1", "testPassword1");
+    }
+
+}
